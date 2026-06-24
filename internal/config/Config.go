82bf4dc56b2a59896be9a7e5f8d92d145c321211
FreakHuhn <mcbabamuck@googlemail.com>
2026-06-24 08:38:12 +0200

    .

--- internal/config/Config.go
+++ internal/config/Config.go
@@ -2,12 +2,33 @@ package config
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 )
 
 type Config struct {
 	Db_Url string `json:"db_url"`
 	Current_user_name string `json:"current_user_name"`
+}
+
+type state struct {
+	ConfigStruct *Config
+}
+
+type command struct {
+	Name []string
+}
+
+type commands struct {
+	Commands map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	if handler, ok := c.Commands[cmd.Name[0]]; ok {
+		return handler(s, cmd)
+	}
+	return errors.New("unknown command")
 }
 
 func(c Config) SetUser(name string) {
@@ -47,4 +68,16 @@ func Read() Config {
 		panic(err)
 	}
 	return config
+}
+
+func handlerLogin(s *state, c command) error {
+	if len(c.Name) == 0 {
+		return errors.New("empty args")
+	}
+	if len(c.Name[0]) == 0 {
+		return errors.New("no name provided")
+	}
+	s.ConfigStruct.SetUser(c.Name[0])
+	fmt.Printf("User %s logged in\n", c.Name[0])
+	return nil
 }
