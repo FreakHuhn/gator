12e17c4cc576cfe26046c47d9bfe7bdd63920d01
FreakHuhn <mcbabamuck@googlemail.com>
2026-06-24 09:25:59 +0200

    updated main logic

--- internal/config/Config.go
+++ internal/config/Config.go
@@ -12,25 +12,33 @@ type Config struct {
 	Current_user_name string `json:"current_user_name"`
 }
 
-type state struct {
+type State struct {
 	ConfigStruct *Config
 }
 
-type command struct {
+type Command struct {
 	Name []string
 }
 
-type commands struct {
-	Commands map[string]func(*state, command) error
+type Commands struct {
+	Commands map[string]func(*State, Command) error
 }
 
-func (c *commands) run(s *state, cmd command) error {
+// Führt den Befehl aus, der dem angegebenen Namen entspricht.
+func (c *Commands) Run(s *State, cmd Command) error {
 	if handler, ok := c.Commands[cmd.Name[0]]; ok {
 		return handler(s, cmd)
 	}
 	return errors.New("unknown command")
 }
 
+// Registriert einen neuen Befehl mit dem angegebenen Namen und der zugehörigen Funktion.
+func (c *Commands) Register(name string, f func(*State, Command) error) {
+	c.Commands[name] = f
+}
+
+// SetUser setzt den aktuellen Benutzernamen in 
+// der Konfiguration und speichert die Konfiguration in einer JSON-Datei.
 func(c Config) SetUser(name string) {
 	c.Current_user_name = name
 	homeLocation, err := os.UserHomeDir()
@@ -50,6 +58,7 @@ func(c Config) SetUser(name string) {
 	}
 }
 
+// Read liest die Konfiguration aus der JSON-Datei und gibt sie zurück.
 func Read() Config {
 	homeLocation, err := os.UserHomeDir()
 	if err != nil {
@@ -70,14 +79,13 @@ func Read() Config {
 	return config
 }
 
-func handlerLogin(s *state, c command) error {
-	if len(c.Name) == 0 {
-		return errors.New("empty args")
+// HandlerLogin ist eine Funktion, die den Login-Befehl verarbeitet.
+func HandlerLogin(s *State, c Command) error {
+	if len(c.Name) < 2 {
+		return errors.New("username required")
 	}
-	if len(c.Name[0]) == 0 {
-		return errors.New("no name provided")
-	}
-	s.ConfigStruct.SetUser(c.Name[0])
-	fmt.Printf("User %s logged in\n", c.Name[0])
+	username := c.Name[1]
+	s.ConfigStruct.SetUser(username)
+	fmt.Printf("User %s logged in\n", username)
 	return nil
 }
--- main.go
+++ main.go
@@ -2,13 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/FreakHuhn/gator/internal/config"
 )
 
 func main() {
+	var state config.State
 	cfg := config.Read()
-	cfg.SetUser("FreakHuhn")
-	cfg = config.Read()
-	fmt.Println(cfg)
+	state.ConfigStruct = &cfg
+	commands := config.Commands{
+		Commands: make(map[string]func(*config.State, config.Command) error),
+	}
+	commands.Register("login", config.HandlerLogin)
+	if len(os.Args) < 2 {
+		fmt.Println("Error: Not enough arguments provided.")
+		os.Exit(1)
+	}
+	cmd := config.Command{
+		Name: os.Args[1:],
+	}
+	err := commands.Run(&state, cmd)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
 }
